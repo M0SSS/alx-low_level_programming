6210cac254e3580f86b8def822bbf2ba4745d633
M0SSS <ultraoppo0@gmail.com>
2022-10-18 06:41:01 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,6 +4,7 @@
  * _islower - Checks for lowercase character
  * @c: The character to be checked
  *
+ * Return: 1 for lowercase character or 0 for anything else
  */
 int _islower(int c)
 {
@@ -14,3 +15,4 @@ int _islower(int c)
 
 	return (0);
 }
+
