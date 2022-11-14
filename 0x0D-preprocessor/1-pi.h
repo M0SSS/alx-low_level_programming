236ed5289c8ef8c57b26b219fc400b37226c5d29
M0SSS <ultraoppo0@gmail.com>
2022-11-14 04:25:41 -0800

    task 2

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef PI_H
+#define PI_h
+
+/*
+ * File: 1-pi.h
+ * Auth: Musse Shiferaw
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
